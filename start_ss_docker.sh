18d699467d287b8ff196b269617bec38424684e6
Neo@vultr <neosfung@gmail.com>
2017-08-12 11:58:51 +0800

    add ss and ssr script

---
+++ start_ss_docker.sh
@@ -0,0 +1,13 @@
+PASSWORD="Pass_word"
+PORT="8988"
+NAME="shadowsocks"
+METHOD="aes-256-cfb"
+
+echo stop and remove shadowsocks instance
+docker stop shadowsocks
+docker rm shadowsocks
+
+echo run the image
+docker run --name $NAME --detach --publish $PORT:8338 --publish $PORT:8838/udp shadowsocks/shadowsocks-libev -k $PASSWORD --fast-open -v -m $METHOD
+docker ps -al
+
