38fff08cce887ae92accc2f32e9c6940e7c714a8
Neo@vultr <neosfung@gmail.com>
2018-01-09 16:01:37 +0800

    update protocol

--- start_ssr_docker.sh
+++ start_ssr_docker.sh
@@ -1,4 +1,5 @@
-docker stop ssr-with-net-speeder
-docker rm ssr-with-net-speeder
-docker run -d --name ssr-with-net-speeder --restart=always -p 8989:8989 malaohu/ssr-with-net-speeder -s 0.0.0.0 -p 8989 -k Pass_word -m aes-256-cfb -o tls1.2_ticket_auth -O auth_sha1_v4
+docker stop ssr-docker
+docker rm ssr-docker
 
+
+docker run -d -p 8989:8989/tcp -p 8989:8989/udp --name ssr-docker jpacg/shadowsocksr -s 0.0.0.0 -p 8989 -k Pass_word -m none -o tls1.2_ticket_auth -O auth_chain_a
