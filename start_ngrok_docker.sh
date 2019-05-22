7c9e39193813b55d76cf545659e3f095dcb038aa
Neo@vultr <neosfung@gmail.com>
2019-05-22 11:33:49 +0800

    update

--- start_ngrok_docker.sh
+++ start_ngrok_docker.sh
@@ -1,2 +1,2 @@
 #docker run --rm -it -e DOMAIN="tunnel.neofung.org" -v /home/neo/shared/softwares/ngrok:/myfiles neofung/ngrok /bin/sh /build.sh
-docker run -idt --name ngrok-server -v /home/neo/shared/softwares/ngrok:/myfiles -p 8082:80 -p 4432:443 -p 4443:4443 -e DOMAIN=tunnel.neofung.org neofung/ngrok /bin/sh /server.sh
+docker run -idt --name ngrok-server -v /home/neo/shared/softwares/ngrok:/myfiles -p 8082:80 -p 4432:443 -p 4443:4443 -p 37871:37871 -e DOMAIN=tunnel.neofung.org neofung/ngrok /bin/sh /server.sh
